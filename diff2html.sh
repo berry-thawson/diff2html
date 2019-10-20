e017396afcd957d45d922f5f4fa629eccde856e0
berry-thawson <sivachalabathid@gmail.com>
2019-10-20 21:59:20 +0530

    Update diff2html.sh
    Added documentation to awk script. Removed a variable.

--- diff2html.sh
+++ diff2html.sh
@@ -6,6 +6,9 @@ file2="${2}"
 
 mawk -v file1_name="${1}" -v file2_name="${2}" 'BEGIN {
 
+	#Printing the header and file names.
+	#css is default now.
+	#had to add funtinality for custom css.
 	print "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\"\
  \"http://www.w3.org/TR/REC-html40/loose.dtd\">\
 <html>\
@@ -38,13 +41,17 @@ mawk -v file1_name="${1}" -v file2_name="${2}" 'BEGIN {
         <th>&nbsp;</th>\
         <th width=\"45%\"><strong><big>",file2_name,"</big></strong></th>\
     </tr>";
+    	#Initializing line counter variables to 0.
 	file1_lc=0;
 	file2_lc=0;
-	diff_line_lc=0;
+	#This is used to initialize an empty array.
+	#This is needed for compatibility with mawk.
+	#Gawk is pretty nice to these.
 	split("", del_arr);
 	mod_index=0;
-	}
+}
 
+#Convert a caertain string chars with certain html chars.
 function str2htm(str_to_replace) {
 	gsub("\\&","\\&amp",str_to_replace);
 	gsub("<","\\&lt",str_to_replace);
@@ -54,6 +61,8 @@ function str2htm(str_to_replace) {
 	return str_to_replace;
 }
 
+#writing file lines as html formatted output with chnge type marked
+#allowed type args - "modified", "added", "removed" and "normal"
 function writeLine(linenum1,line1,linenum2,line2,type1,type2) {
 	print "\t<tr>";
 	print "\t\t<td class=\"linenum\">" linenum1 "</td>";
@@ -64,6 +73,14 @@ function writeLine(linenum1,line1,linenum2,line2,type1,type2) {
 	print "\t</tr>";
 }
 
+#diff line input is labelled as "marker line_input" where,
+#marker can be +, - and *.
+
+#Saving the html_str in del_arr.
+#This is because only when next block after deletion comes,
+#we can decide if the current block is deletion or modified.
+#The length should be tracked manually in mawk. gawk offers
+#a way to find length, but we use mawk way to have compatibility
 /^\-/{ 
 	diff_str=substr($0,2);
 	html_str=str2htm(diff_str);
@@ -71,6 +88,7 @@ function writeLine(linenum1,line1,linenum2,line2,type1,type2) {
 	array_len++;
 }
 
+#We first write the del_arr output, and then write the normal output.
 /^\*/{
 	diff_str=substr($0,2);
 	html_str=str2htm(diff_str);
@@ -84,6 +102,9 @@ function writeLine(linenum1,line1,linenum2,line2,type1,type2) {
 	writeLine(file1_lc++,html_str,file2_lc++,html_str,"normal","normal");
 }
 
+#When we encounter a added block, if del_arr is not empty, we write 
+#both  together as modified block. Else, if del_arr is empty, then
+#write the line input as added block.
 /^\+/{ 
 	diff_str=substr($0,2);
 	html_str=str2htm(diff_str);
